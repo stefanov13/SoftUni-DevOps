e1eafe0cb7fa7e8f237fa4faea0b6faf0f04ec4a
Evgeni Stefanov <102958099+stefanov13@users.noreply.github.com>
2023-11-01 12:12:26 +0200

    Initial upload

--- 08_Exercise-IaC-and-Monitoring/Resources/01_azure_resource_group/main.tf
+++ 08_Exercise-IaC-and-Monitoring/Resources/01_azure_resource_group/main.tf
@@ -1,1 +1,20 @@
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
 
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  skip_provider_registration = true # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
+  features {}
+}
+
+resource "azurerm_resource_group" "myrg" {
+  name     = "my-example-rg"
+  location = "West Europe"
+}
